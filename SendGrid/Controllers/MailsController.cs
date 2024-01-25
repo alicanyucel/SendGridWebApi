1e59e06081466d0f471c69f1cf00acc20fa91585
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-01-25 23:39:26 +0300

    SenfGridEmail

---
+++ SendGrid/Controllers/MailsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using System.ComponentModel.DataAnnotations;
+using SendGrid.Helpers.Mail;
+namespace SendGrid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> SendEmail()
+        {
+            var apiKey = "apikey alıp yazıyoruz";
+            var client = new SendGridClient(apiKey);
+            var from = new EMailAddress("gonderii emaila adresi", "isim soy isim");
+            var subject = "konuyu yazıyoruyz";
+            var to = new EmailAddress("kimemnail atcaksak", "kime gidecek ");
+            var plainTextContext = "mesajımız";
+            var htmlContent = "<storng>html içeriğinde de yazı gönderebiliriz<strong>";
+            var msg=MailHelper.CreateSingleEmail(from,to,subject,plainTextContext,htmlContent);
+            var response=await client.SendAsync(msg);
+            return Ok(new { Message = "mail başarılı gitti" });
+
+        }
+
+    }
+}
